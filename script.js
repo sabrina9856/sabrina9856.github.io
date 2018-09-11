233d8f1c97bdf1bb0ad9c42dbd305cfe86845aac
sabrina9856 <40377838+sabrina9856@users.noreply.github.com>
2018-09-11 14:59:20 -0400

    Changes
    I did it!!

---
+++ script.js
@@ -0,0 +1,19 @@
+/**
+ * @copyright Sabrina
+ */
+
+var docBody = document.body;
+let variable = '';
+docBody.addEventListener()
+
+/**
+ * Bob is simple function that returns always true.
+ * 
+ * @return It returns a boolean and it is true
+ */
+function bob()  {
+    var bob;
+    return true;
+}
+
+bob();
